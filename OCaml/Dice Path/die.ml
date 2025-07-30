75b5b8a5dcc485e7e904fad711520cd8cf7bd4f2
mindedmac <178513685+mindedmac@users.noreply.github.com>
2025-07-29 23:15:46 -0400

    jyj4uk

--- OCaml/Dice Path/die.ml
+++ OCaml/Dice Path/die.ml
@@ -7,30 +7,3 @@ let mface: int list = [1;2;6;5]
 let position = (1, 1)
 let top = 1
 
-
-(*
-
-let go (l: int list) (pos: int * int) =
-  match pos with
-  | (m, n) when (m = fst position) && (n = snd position) -> 
-  | (m,n) when (m >= 1) && (n >= 1) && (m + n <= 120) -> 
-  | (_,_) -> failwith "Invalid position"
-
-*)
-
-
-
-let rec right (n: int) =
-  for i = 1 to n do
-    let new_top = List.nth nface (top - 1)
-    let new_pos = (fst position, snd position + 1)
-    go nface new_pos
-  done
-
-let rec down (n: int) =
-  for i = 1 to n do
-    let new_top = List.nth mface (top - 1)
-    let new_pos = (fst position + 1, snd position)
-    go mface new_pos
-  done
-
