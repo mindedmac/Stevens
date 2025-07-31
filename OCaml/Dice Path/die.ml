0f1772a72fc596fb95250a9e08e1650fab7e1e14
mindedmac <178513685+mindedmac@users.noreply.github.com>
2025-07-31 10:06:44 -0400

    r&d2

--- OCaml/Dice Path/die.ml
+++ OCaml/Dice Path/die.ml
@@ -1,13 +1,16 @@
-let mface = ref [|5; 6; 2; 1|]  (* [front; bottom; back; top] *)
-let nface = ref [|2; 4; 5; 3|]  (* [front; right; back; left] *)
+
+
+let mface = ref [|5; 6; 2; 1|]  (* [back; bottom; front; top] *)
+let nface = ref [|3; 6; 4; 1|] 
 let position = ref (1, 1)
 let top = ref 1
 let bottom = ref 6
 
 let down () =
   let om = !mface in
+  let on = !nface in
   mface := [| om.(1); om.(2); om.(3); om.(0) |];
-  nface := [| !mface.(0); !nface.(1); !mface.(2); !nface.(3) |];
+  nface := [| om.(0); on.(3); om.(2); on.(1) |];
   position := (fst !position + 1, snd !position);
   top := !mface.(3);
   bottom := !mface.(1)
@@ -15,10 +18,9 @@ let down () =
 let right () =
   let om = !mface in
   let on = !nface in
-  mface := [| om.(0); on.(1); om.(2); on.(3) |];
-  nface := [| on.(0); om.(3); on.(2); om.(1) |];
+  mface := [| om.(0); on.(2); om.(2); on.(0) |];
+  nface := [| on.(1); on.(2); on.(3); on.(0) |];
   position := (fst !position, snd !position + 1);
   top := !mface.(3);
   bottom := !mface.(1)
 
-  
